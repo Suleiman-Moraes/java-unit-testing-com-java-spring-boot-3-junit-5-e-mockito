8d0dc01699492283b5baecdf22d65f073194414c
suleiman-moraes <suleimanmoraes@gmail.com>
2023-11-05 19:36:26 +0000

    Finish

---
+++ S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/readme.md
@@ -0,0 +1,53 @@
+# Example Project with Spring Boot and Java
+
+This is an example project that demonstrates the use of Spring Boot and Java to create a web application. The application uses several dependencies and plugins from the Maven ecosystem to configure the project.
+
+## Project Details
+
+- **Project Name:** rest-with-spring-boot-and-java-erudio
+- **Description:** Demo project using Spring Boot
+- **Version:** 0.0.1-SNAPSHOT
+
+## Technologies and Dependencies
+
+The project uses the following technologies and dependencies:
+
+- **Spring Boot:** The project is based on Spring Boot, a Java framework for creating web applications.
+- **Java:** The project is developed in Java 17.
+- **Spring Data JPA:** Spring Data JPA is used to integrate with databases.
+- **MySQL and PostgreSQL:** MySQL and PostgreSQL databases are supported.
+- **Springdoc OpenAPI:** The Springdoc OpenAPI dependency is used for generating OpenAPI documentation.
+- **Rest Assured:** The Rest Assured library is used for API testing.
+- **Testcontainers:** Testcontainers is used for testing containers in PostgreSQL databases.
+
+## How to Execute the Project
+
+To run the project, follow these steps:
+
+1. Make sure Java 17 is installed in your environment.
+
+2. Clone the project repository to your local machine:
+
+    ```shell
+     git clone https://github.com/Suleiman-Moraes/java-unit-testing-com-java-spring-boot-3-junit-5-e-mockito.git
+     ```
+
+3. Navigate to the project directory:
+
+     ```shell
+     cd java-unit-testing-com-java-spring-boot-3-junit-5-e-mockito\S10_TestingRepositories\rest-with-spring-boot-and-java-erudio
+     ```
+
+4. Run the project using Maven:
+
+     ```shell
+     mvn spring-boot:run
+     ```
+
+The application will launch and be available at http://localhost:8080. You can access the API documentation at http://localhost:8080/swagger-ui.html.
+
+Contributions
+Contributions are welcome. Feel free to collaborate, report problems or suggest improvements to the project.
+
+License
+This project is licensed under [Your License](License URL). See the LICENSE.md file for more details.
--- S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integration/controller/PersonControllerIntegrationTest.java
+++ S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integration/controller/PersonControllerIntegrationTest.java
@@ -4,6 +4,9 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -22,6 +25,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 @TestMethodOrder(OrderAnnotation.class)
@@ -31,6 +35,7 @@ class PersonControllerIntegrationTest extends AbstractIntegrationTest {
     private static RequestSpecification specification;
     private static ObjectMapper mapper;
     private static Person person;
+    private Long id2 = 0l;
 
     /**
      * Initializes the necessary objects and configurations for the test.
@@ -58,15 +63,7 @@ class PersonControllerIntegrationTest extends AbstractIntegrationTest {
     @Test
     @Order(1)
     void testIntegrationGivenObject_When_CreateOnePerson_ShouldReturnObject() throws Exception {
-        var response = given()
-                .spec(specification)
-                .contentType(TestConfig.CONTENT_TYPE_JSON)
-                .body(person)
-                .when()
-                .post();
-
-        // Verify that the response status code is 200 OK
-        response.then().statusCode(200);
+        var response = create(person);
 
         // Extract the response body as a string
         Person createdPerson = mapper.readValue(response.getBody().asString(), Person.class);
@@ -84,5 +81,157 @@ class PersonControllerIntegrationTest extends AbstractIntegrationTest {
         assertEquals("susu@susu.com.br", createdPerson.getEmail());
         assertEquals("Uberlandia - Minas Gerais - Brasil", createdPerson.getAddress());
         assertEquals("Male", createdPerson.getGender());
+    }
+
+    private Response create(Person person) {
+        var response = given()
+                .spec(specification)
+                .contentType(TestConfig.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .post();
+
+        // Verify that the response status code is 200 OK
+        response.then().statusCode(200);
+        return response;
+    }
+
+    @DisplayName("JUnit form Given Object When Updated Then Return Saved Object")
+    @Test
+    @Order(2)
+    void testIntegrationGivenObject_When_UpdateOnePerson_ShouldReturnObject() throws Exception {
+        final String firstName = "Suleiman";
+        final String lastName = "Alves de Moraes";
+        final String address = "Goiânia - Goiás - Brasil";
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setAddress(address);
+        var response = given()
+                .spec(specification)
+                .contentType(TestConfig.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .put();
+
+        // Verify that the response status code is 200 OK
+        response.then().statusCode(200);
+
+        // Extract the response body as a string
+        Person createdPerson = mapper.readValue(response.getBody().asString(), Person.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getEmail());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+        assertEquals(firstName, createdPerson.getFirstName());
+        assertEquals(lastName, createdPerson.getLastName());
+        assertEquals("susu@susu.com.br", createdPerson.getEmail());
+        assertEquals(address, createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+    }
+
+    @DisplayName("JUnit form Given Id When FindById Then Return Saved Object")
+    @Test
+    @Order(3)
+    void testIntegrationGivenId_When_FindByIdOnePerson_ShouldReturnObject() throws Exception {
+        final String firstName = "Suleiman";
+        final String lastName = "Alves de Moraes";
+        final String address = "Goiânia - Goiás - Brasil";
+        var response = given()
+                .spec(specification)
+                .pathParam("id", person.getId())
+                .when()
+                .get("{id}");
+
+        // Verify that the response status code is 200 OK
+        response.then().statusCode(200);
+
+        // Extract the response body as a string
+        Person person = mapper.readValue(response.getBody().asString(), Person.class);
+
+        assertNotNull(person);
+        assertNotNull(person.getId());
+        assertNotNull(person.getFirstName());
+        assertNotNull(person.getLastName());
+        assertNotNull(person.getEmail());
+        assertNotNull(person.getAddress());
+        assertNotNull(person.getGender());
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
+        assertEquals("susu@susu.com.br", person.getEmail());
+        assertEquals(address, person.getAddress());
+        assertEquals("Male", person.getGender());
+    }
+
+    @DisplayName("JUnit form When FindAll Then Return List of Object")
+    @Test
+    @Order(4)
+    void testIntegration_When_FindAll_ShouldReturnObject() throws Exception {
+        create(new Person("Susu", "Moraes", "susu2@susu.com.br", "Uberlandia - Minas Gerais - Brasil", "Male"));
+        final String firstName = "Suleiman";
+        final String lastName = "Alves de Moraes";
+        final String address = "Goiânia - Goiás - Brasil";
+        var response = given()
+                .spec(specification)
+                .when()
+                .get();
+
+        // Verify that the response status code is 200 OK
+        response.then().statusCode(200);
+
+        // Extract the response body as a string
+        List<Person> persons = Arrays.asList(mapper.readValue(response.getBody().asString(), Person[].class));
+
+        Person person1 = persons.get(0);
+        Person person2 = persons.get(1);
+        id2 = person2.getId();
+        delete(id2);
+
+        assertNotNull(person1);
+        assertNotNull(person1.getId());
+        assertNotNull(person1.getFirstName());
+        assertNotNull(person1.getLastName());
+        assertNotNull(person1.getEmail());
+        assertNotNull(person1.getAddress());
+        assertNotNull(person1.getGender());
+        assertEquals(firstName, person1.getFirstName());
+        assertEquals(lastName, person1.getLastName());
+        assertEquals("susu@susu.com.br", person1.getEmail());
+        assertEquals(address, person1.getAddress());
+
+        assertNotNull(person2);
+        assertNotNull(person2.getId());
+        assertNotNull(person2.getFirstName());
+        assertNotNull(person2.getLastName());
+        assertNotNull(person2.getEmail());
+        assertNotNull(person2.getAddress());
+        assertNotNull(person2.getGender());
+        assertEquals("Susu", person2.getFirstName());
+        assertEquals("Moraes", person2.getLastName());
+        assertEquals("susu2@susu.com.br", person2.getEmail());
+        assertEquals("Uberlandia - Minas Gerais - Brasil", person2.getAddress());
+    }
+
+    @DisplayName("JUnit form Given Id When Delete Then Return No Content")
+    @Test
+    @Order(5)
+    void testIntegrationGivenId_When_DeleteOnePerson_ShouldReturnNoContent() throws Exception {
+        var response = delete(person.getId());
+
+        // Verify that the response status code is 204 No Content
+        response.then().statusCode(204);
+    }
+
+    private Response delete(Long id) {
+        var response = given()
+                .spec(specification)
+                .pathParam("id", id)
+                .when()
+                .delete("{id}");
+        return response;
     }
 }
